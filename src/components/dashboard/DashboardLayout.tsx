0fb56cca4d6a7281669beb2fe9d1047e2f985240
Stephen Wright <stephen.wright@teooh.com>
2021-04-09 16:02:15 +0100

    query server on component load

--- src/components/dashboard/DashboardLayout.tsx
+++ src/components/dashboard/DashboardLayout.tsx
@@ -1,7 +1,25 @@
-import * as React from 'react'
+import { useEffect } from 'react'
 
-export const DashboardLayout = () => (
-    <div>
-        <p>Dashboard</p>
-    </div>
-)
+export const DashboardLayout = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch("/system/dashboard", {
+                method: 'GET'
+            }).then(res => {
+                if(res.ok) {
+                    res.text().then(data => {
+                        console.log(data)
+                    })
+                } else {
+                    console.log('BAD')
+                }
+            })
+        }
+        fetchData()
+    },[])
+    return (
+        <div>
+            <p>Dashboard</p>
+        </div>
+    )
+}
